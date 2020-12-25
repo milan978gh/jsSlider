2c2c0d2c4f07222b6fa25ec6013f50493a0218bf
milan978gh <amilandev78@gmail.com>
2020-12-25 12:49:41 +0100

    update

--- js/main.js
+++ js/main.js
@@ -3,20 +3,44 @@ const prevBtn = document.querySelector('.prevBtn');
 const nextBtn = document.querySelector('.nextBtn');
 let counter = 0;
 
+// slide stacking done from JS - dynamically
 slides.forEach((slide, index) => {
   slide.style.left = `${index * 100}%`;
 });
 
 nextBtn.addEventListener('click', () => {
   counter++;
+  carousel();
 });
 
-prevBtnBtn.addEventListener('click', () => {
+prevBtn.addEventListener('click', () => {
   counter--;
+  carousel();
 });
 
 function carousel() {
+  // working with slides in a loop
+  /*if (counter === slides.length) {
+    counter = 0;
+  }
+  if (counter < 0) {
+    counter = slides.length - 1;
+  }*/
+
+  // working with buttons
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = 'block';
+  } else {
+    nextBtn.style.display = 'none';
+  }
+  if (counter > 0) {
+    prevBtn.style.display = 'block';
+  } else {
+    prevBtn.style.display = 'none';
+  }
+
   slides.forEach((slide) => {
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 }
+prevBtn.style.display = 'none';
